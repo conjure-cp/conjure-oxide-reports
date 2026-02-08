830a5d5f78a7788ef1adcb58f1a2b8b7c1df9412
JamieASM <JamieASM@users.noreply.github.com>
2026-02-08 15:11:58 +0000

    Actions: Deploy docs for pr-1358

--- pr/1358/book/searcher-c2a407aa.js
+++ pr/1358/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c4e34754.js',
+                path_to_root + 'searchindex-0ba53dba.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
