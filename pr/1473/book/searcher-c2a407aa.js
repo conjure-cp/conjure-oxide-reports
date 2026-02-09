9b86accd8949d2b5e8cc573f3e31e3d91909fc61
HKhan-5 <HKhan-5@users.noreply.github.com>
2026-02-09 12:54:48 +0000

    Actions: Deploy docs for pr-1473

--- pr/1473/book/searcher-c2a407aa.js
+++ pr/1473/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-393733bd.js',
+                path_to_root + 'searchindex-78c1d72e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
