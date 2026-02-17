d6df2d99454c0bb2ec62272be21f32e9d4f1c07e
ND65StA <ND65StA@users.noreply.github.com>
2026-02-17 16:43:26 +0000

    Actions: Deploy docs for pr-1349

--- pr/1349/book/searcher-c2a407aa.js
+++ pr/1349/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-20b929c8.js',
+                path_to_root + 'searchindex-f48cba70.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
