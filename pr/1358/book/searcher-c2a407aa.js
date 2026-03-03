2d4805309ed6615a10ecb5aecb41f97a0b97c864
chococara-exe <chococara-exe@users.noreply.github.com>
2026-03-03 21:24:03 +0000

    Actions: Deploy docs for pr-1358

--- pr/1358/book/searcher-c2a407aa.js
+++ pr/1358/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c6eb60b7.js',
+                path_to_root + 'searchindex-b402ff7f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
