64d889af78ca1074c348dee9c43b98ae4429c19d
htbrown <htbrown@users.noreply.github.com>
2026-03-09 15:58:32 +0000

    Actions: Deploy docs for pr-1558

--- pr/1558/book/searcher-c2a407aa.js
+++ pr/1558/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5b39acce.js',
+                path_to_root + 'searchindex-4c0a2262.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
