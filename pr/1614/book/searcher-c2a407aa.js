1fac89aa146e0f09102b356800f76858e183364d
S-andres0694 <S-andres0694@users.noreply.github.com>
2026-03-10 13:42:40 +0000

    Actions: Deploy docs for pr-1614

--- pr/1614/book/searcher-c2a407aa.js
+++ pr/1614/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c7127b80.js',
+                path_to_root + 'searchindex-754f990a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
