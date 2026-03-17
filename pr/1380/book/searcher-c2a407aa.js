772f9f897fec76f479e4a686e16d3e062a4db80e
EdwardTheLegend <EdwardTheLegend@users.noreply.github.com>
2026-03-17 14:48:23 +0000

    Actions: Deploy docs for pr-1380

--- pr/1380/book/searcher-c2a407aa.js
+++ pr/1380/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-11b36c88.js',
+                path_to_root + 'searchindex-f5ad4913.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
