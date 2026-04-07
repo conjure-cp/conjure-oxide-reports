22c8ff31dc82faa0adcba8e8be517715b7a46787
EEJDempster <EEJDempster@users.noreply.github.com>
2026-04-07 13:57:49 +0000

    Actions: Deploy docs for pr-1722

--- pr/1722/book/searcher-c2a407aa.js
+++ pr/1722/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c99d9f18.js',
+                path_to_root + 'searchindex-e23bf53e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
