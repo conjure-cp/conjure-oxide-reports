24d393df38fe1bdc32424dc83ce5398d2df08e6b
ozgurakgun <ozgurakgun@gmail.com>
2026-04-21 08:14:47 +0000

    Actions: Deploy docs for main

--- main/book/searcher-c2a407aa.js
+++ main/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-adb09803.js',
+                path_to_root + 'searchindex-69c3ef39.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
