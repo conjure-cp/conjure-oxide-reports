559f88db7dda1c53c2b7763e8a5d094a44cdc838
ND65StA <ND65StA@users.noreply.github.com>
2026-04-24 15:18:20 +0000

    Actions: Deploy docs for pr-1779

--- pr/1779/book/searcher-c2a407aa.js
+++ pr/1779/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c72fcf7b.js',
+                path_to_root + 'searchindex-3978239e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
