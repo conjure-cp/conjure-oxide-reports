a136a9310a4fb98e5d090d452226e563021652a6
ca1umac <ca1umac@users.noreply.github.com>
2026-04-26 10:21:38 +0000

    Actions: Deploy docs for pr-1716

--- pr/1716/book/searcher-c2a407aa.js
+++ pr/1716/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c7d4e386.js',
+                path_to_root + 'searchindex-0f40724d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
