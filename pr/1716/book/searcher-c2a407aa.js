2031010bffb395150c4ea273894d2bafb20f497d
HKhan-5 <HKhan-5@users.noreply.github.com>
2026-05-03 23:26:41 +0000

    Actions: Deploy docs for pr-1716

--- pr/1716/book/searcher-c2a407aa.js
+++ pr/1716/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4a8f4ee1.js',
+                path_to_root + 'searchindex-c83c281f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
