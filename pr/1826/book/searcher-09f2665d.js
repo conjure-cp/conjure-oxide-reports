cc472ecb13ee2774ee6bc07b357577cbad4d07ec
YehorBoiar <YehorBoiar@users.noreply.github.com>
2026-05-23 14:00:25 +0000

    Actions: Deploy docs for pr-1826

--- pr/1826/book/searcher-09f2665d.js
+++ pr/1826/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-58324deb.js',
+                path_to_root + 'searchindex-8cf6b077.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
