c963d9cbaa8bf227c73f86146e5de3bab58898eb
leiamcalisteryoung <leiamcalisteryoung@users.noreply.github.com>
2026-05-23 16:56:45 +0000

    Actions: Deploy docs for pr-1832

--- pr/1832/book/searcher-09f2665d.js
+++ pr/1832/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2d66b0e4.js',
+                path_to_root + 'searchindex-c0977a0c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
