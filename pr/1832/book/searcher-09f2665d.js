4dc7728cc122d8419f7deef267c3a10b4774d5b9
Soph1514 <Soph1514@users.noreply.github.com>
2026-05-25 13:29:17 +0000

    Actions: Deploy docs for pr-1832

--- pr/1832/book/searcher-09f2665d.js
+++ pr/1832/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-dba5adcd.js',
+                path_to_root + 'searchindex-f9142a12.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
