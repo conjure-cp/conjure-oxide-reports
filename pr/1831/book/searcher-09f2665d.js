5a7515fc5b62995f6c747895be24c65ce9cf9677
a-nosnitram <a-nosnitram@users.noreply.github.com>
2026-05-25 15:46:31 +0000

    Actions: Deploy docs for pr-1831

--- pr/1831/book/searcher-09f2665d.js
+++ pr/1831/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-dd0e4e0d.js',
+                path_to_root + 'searchindex-af727915.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
