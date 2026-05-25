e316df38d45e312e3139dcff411f77b5bfad6058
HKhan-5 <HKhan-5@users.noreply.github.com>
2026-05-25 13:56:45 +0000

    Actions: Deploy docs for pr-1406

--- pr/1406/book/searcher-09f2665d.js
+++ pr/1406/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c1d1492d.js',
+                path_to_root + 'searchindex-4675097a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
