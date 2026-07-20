f73c312f2bfb225a2b2b26fc6c33bc970cab9802
ozgurakgun <ozgurakgun@users.noreply.github.com>
2026-07-20 14:03:31 +0000

    Actions: Deploy docs for pr-1893

--- pr/1893/book/searcher-09f2665d.js
+++ pr/1893/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-18bf8fba.js',
+                path_to_root + 'searchindex-f66d81f1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
