c148c3e3db4b2d2b2fbaacc4774a60935a2e3401
Shikhar-Srivastava-16 <Shikhar-Srivastava-16@users.noreply.github.com>
2026-09-18 21:05:39 +0000

    Actions: Deploy docs for pr-1907

--- pr/1907/book/searcher-09f2665d.js
+++ pr/1907/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-28911750.js',
+                path_to_root + 'searchindex-90e5e0f6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
